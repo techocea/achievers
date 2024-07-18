269e89d6d3b9c9bf28410b08d929800ee2e27581
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-18 13:29:28 +0530

    seee

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors({
-        origin: "https://achievers-server.vercel.app"
-    }
-))
-app.options('*', cors())
+//cors middleware
+const corsOption = {
+  origin: ["http://localhost:3000", "https://achievers-client.vercel.app"],
+  Credentials: true,
+};
+app.use(cors(corsOption));
 
 // Connect to the database
 connectToDB();
