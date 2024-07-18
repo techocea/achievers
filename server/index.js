e78d34edfa46bb933326c107b3b4fd9a89304ffb
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-18 13:37:02 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -14,11 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //cors middleware
-const corsOption = {
-  origin: ["http://localhost:3000", "https://achievers-client.vercel.app"],
-  Credentials: true,
-};
-app.use(cors(corsOption));
+app.use(cors());
 
 // Connect to the database
 connectToDB();
