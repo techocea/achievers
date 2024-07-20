ac61186023f49296aa493cb7e5fecfd1e435b9a6
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-20 21:43:46 +0530

    see more

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const connectToDB = require("./db/connectToDB");
-const formRoutes = require("./routes/formRoutes");
+const connectToDB = require('./db/connectToDB');
+const formRoutes = require('./routes/formRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//cors middleware
+// CORS middleware
 const corsOptions = {
-  origin: "https://achievers-client.vercel.app",
-  methods: ["GET,POST"],
-  allowedHeaders: "Content-Type,Authorization",
+  origin: 'https://achievers-client.vercel.app', // Your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Ensure all methods are allowed if needed
+  allowedHeaders: ['Content-Type', 'Authorization'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
@@ -28,10 +28,10 @@ app.use(cors(corsOptions));
 connectToDB();
 
 // Import routes
-app.use("/api", formRoutes);
+app.use('/api', formRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello Negombo Smart Achievers");
+app.get('/', (req, res) => {
+  res.send('Hello Negombo Smart Achievers');
 });
 
 app.listen(PORT, () => {
