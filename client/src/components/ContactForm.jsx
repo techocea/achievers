fd12efc5bfadf689ed6264f4677cf9e0e6ef4d94
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-31 22:53:55 +0530

    Update ContactForm.jsx 2

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -43,7 +43,7 @@ export default function ContactForm() {
     }
 
     try {
-      const response = await fetch('https://achievers-server.vercel.app/register', {
+      const response = await fetch('https://achievers-server.vercel.app/v1/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
