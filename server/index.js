fb52d159930a26e384f089e0e9ff2f97baa1a705
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-31 21:53:55 +0530

    Update index.js 6

--- server/index.js
+++ server/index.js
@@ -12,15 +12,17 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+  next();
+});
 
-app.use(
-  cors({
-    origin: 'https://achievers-client.vercel.app',
-    methods: ['GET','HEAD','PATCH', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials:true,
-  })
-);
+app.get('/', (req, res) => {
+  res.send('CORS is enabled for all origins');
+});
+
 
 // Connect to the database
 connectToDB();
