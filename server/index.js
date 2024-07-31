338b6f30a903e7b9f35594d9f87c39096ecd376e
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-31 23:08:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Origin', 'https://achievers-client.vercel.app'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
   next();
