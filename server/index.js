1a7aa2b3391b90af1eea43ed41c89033ba573adf
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-31 09:52:11 +0530

    Update index.js 3

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
