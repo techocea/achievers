ee60e1f907a404c1e4c01b8948d399d3914ebb26
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2024-07-31 09:40:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // CORS middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 // Connect to the database
 connectToDB();
